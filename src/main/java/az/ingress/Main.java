471fd0d8ab277366d717177caf322e6c9d4703a4
rouvsen <rouvsen@gmail.com>
2024-03-31 16:53:17 +0400

    -

--- src/main/java/az/ingress/Main.java
+++ src/main/java/az/ingress/Main.java
@@ -1,11 +1,7 @@
 package az.ingress;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        System.out.println("Hello world!");//
     }
 }
